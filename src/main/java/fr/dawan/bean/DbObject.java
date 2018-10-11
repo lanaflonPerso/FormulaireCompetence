e2467dcf6cb29c20796af7186ccf3efab0ac8045
Pierre <pierre.mallevaey@laposte.net>
2018-10-11 15:57:39 +0200

    add DbObject
    class for persistable objects

---
+++ src/main/java/fr/dawan/bean/DbObject.java
@@ -0,0 +1,55 @@
+package fr.dawan.bean;
+
+/**
+ * The Class DbObject.
+ * Class for persistable Objects.
+ * @author Pierre
+ */
+public abstract class DbObject {
+	
+	/** The id. */
+	private int id;
+	
+	/** The name. */
+	private String name;
+	
+	
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
