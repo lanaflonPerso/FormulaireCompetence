79f9111bc61674adb0cdd8913da3c4f181940526
christophe <chfontaine.formation@gmail.com>
2018-10-12 12:06:36 +0200

    add administrator attribut => Professor

--- src/main/java/fr/dawan/bean/Professor.java
+++ src/main/java/fr/dawan/bean/Professor.java
@@ -2,7 +2,31 @@ package fr.dawan.bean;
 
 import javax.persistence.Entity;
 
+/**
+ * The Class Professor.
+ */
 @Entity
 public class Professor extends Person{
+	
+	/** The administrator. */
+	private boolean administrator;
+
+	/**
+	 * Checks if is administrator.
+	 *
+	 * @return true, if is administrator
+	 */
+	public boolean isAdministrator() {
+		return administrator;
+	}
+
+	/**
+	 * Sets the administrator.
+	 *
+	 * @param administrator the new administrator
+	 */
+	public void setAdministrator(boolean administrator) {
+		this.administrator = administrator;
+	}
 
 }
