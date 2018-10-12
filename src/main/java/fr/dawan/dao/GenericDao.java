da41d783ee328385c1138d837bb73de6c34e17db
christophe <chfontaine.formation@gmail.com>
2018-10-12 09:39:38 +0200

    add generic dao

---
+++ src/main/java/fr/dawan/dao/GenericDao.java
@@ -0,0 +1,98 @@
+package fr.dawan.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import fr.dawan.bean.DbObject;
+
+/**
+ * The Class GenericDao.
+ */
+public class GenericDao {
+
+	/**
+	 * Save or update.
+	 *
+	 * @param <T> the generic type
+	 * @param item the item
+	 * @param em the em
+	 * @param closeConnection the close connection
+	 */
+	public <T extends DbObject> void saveOrUpdate(T item,EntityManager em,boolean closeConnection) {
+		EntityTransaction tx=em.getTransaction();
+		try {
+			tx.begin();
+			if(item.getId()==0)
+				em.persist(item);
+			else
+				em.merge(item);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}
+		if(closeConnection)
+			em.close();
+	}
+	
+	/**
+	 * Delete.
+	 *
+	 * @param <T> the generic type
+	 * @param item the item
+	 * @param em the em
+	 * @param closeConnection the close connection
+	 */
+	public <T extends DbObject> void delete(T item,EntityManager em,boolean closeConnection) {
+		EntityTransaction tx=em.getTransaction();
+		try {
+			tx.begin();
+			if(item.getId()!=0)
+				em.remove(item);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}
+		if(closeConnection)
+			em.close();
+	}
+	
+	/**
+	 * Find by id.
+	 *
+	 * @param <T> the generic type
+	 * @param clazz the clazz
+	 * @param id the id
+	 * @param em the em
+	 * @param closeConnection the close connection
+	 * @return the t
+	 */
+	public  <T extends DbObject>  T findById(Class<T> clazz, int id,EntityManager em,boolean closeConnection) {
+		T item=em.find(clazz,id);
+		if(closeConnection)
+			em.close();
+		return item;
+	}
+	
+	/**
+	 * Find all.
+	 *
+	 * @param <T> the generic type
+	 * @param clazz the clazz
+	 * @param em the em
+	 * @param begin the begin
+	 * @param end the end
+	 * @param closeConnection the close connection
+	 * @return the list
+	 */
+	public <T extends DbObject>  List<T> findAll(Class<T> clazz,EntityManager em,int begin,int end,boolean closeConnection) {
+		TypedQuery<T> query=em.createQuery("select entity from "+clazz.getName() + " entity",clazz);
+		List<T> result=query.setFirstResult(begin).setMaxResults(end).getResultList();
+		if(closeConnection)
+			em.close();
+		return result;
+	}
+	
+}
