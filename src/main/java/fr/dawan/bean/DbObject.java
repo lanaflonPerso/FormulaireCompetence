02ea1a1140629f4ab5d922d7828df208197ef6b9
Pierre <pierre.mallevaey@laposte.net>
2018-10-12 10:55:00 +0200

    add Hashcode and equals on dbObject

--- src/main/java/fr/dawan/bean/DbObject.java
+++ src/main/java/fr/dawan/bean/DbObject.java
@@ -1,5 +1,9 @@
 package fr.dawan.bean;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * The Class DbObject.
  * Class for persistable Objects.
@@ -8,12 +12,14 @@ package fr.dawan.bean;
 public abstract class DbObject {
 	
 	/** The id. */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	/** The name. */
 	private String name;
 	
-	
+	//********************Getters / Setters******************
 	/**
 	 * Gets the id.
 	 *
@@ -49,7 +55,35 @@ public abstract class DbObject {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	
+	//******************** Others ********************
 	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DbObject other = (DbObject) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
